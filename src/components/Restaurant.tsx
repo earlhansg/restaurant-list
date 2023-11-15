80f856b679ab7a512af46c73ed4369b63b468952
Earl Hans Genoso <earlhansg@gmail.com>
2023-11-16 03:56:32 +0800

    Utilize the API to retrieve data and store it in the state, from which the child component RestaurantList will access the data using the associated state. We will proceed to address the bug identified in the UI.

--- src/components/Restaurant.tsx
+++ src/components/Restaurant.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useReducer, useState } from "react";
 import axios from "axios";
 import { RestaurantList } from "./RestaurantList";
 
-type RestaurantProps = {
+export type RestaurantProps = {
   restaurant_name: string;
   state: string;
 };
@@ -10,42 +10,58 @@ type RestaurantProps = {
 type InitialDataProps = {
   loading: boolean;
   data: RestaurantProps[];
+  state: string[];
   error: boolean;
 };
 
 const InitialData = {
   loading: false,
   data: [],
+  state: [],
   error: false,
 };
 
 export const Restaurant = () => {
   const [data, setData] = useState<InitialDataProps>(InitialData);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await axios.get<RestaurantProps[]>("https://nextjs-orpin-omega-98.vercel.app/api/restaurants");
-  //       setData((prev: InitialDataProps) => ({
-  //         ...prev,
-  //         loading: true,
-  //         data: [...response.data],
-  //       }));
-  //     } catch {
-  //       setData((prev: InitialDataProps) => ({ ...prev, error: true }));
-  //     } finally {
-  //       setData((prev: InitialDataProps) => ({ ...prev, loading: false }));
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<RestaurantProps[]>("https://nextjs-orpin-omega-98.vercel.app/api/restaurants");
+        setData((prev: InitialDataProps) => ({
+          ...prev,
+          loading: true,
+          data: [...response.data],
+          state: returnAllState(response.data)
+        }));
+      } catch {
+        setData((prev: InitialDataProps) => ({ ...prev, error: true }));
+      } finally {
+        setData((prev: InitialDataProps) => ({ ...prev, loading: false }));
+      }
+    };
+    fetchData();
+  }, [data]);
+
+  const returnAllState = (data: RestaurantProps[]) => {
+    return Object.keys(data.reduce((acc: any, curr) => {
+      const state = curr.state;
+      acc[state] = (acc[state] || 0) + 1;
+      return acc;
+    }, {}));
+  }
+
+  const filterByState = (state: string) => {
+    return data.data.filter((restaurant) => restaurant.state === state)
+  }
+  
 
   return (
     <div>
-      <RestaurantList />
-      {/* {data.data.map((value) => (
-        <p key={value.restaurant_name}>{value.restaurant_name}</p>
-      ))} */}
+      {/* <RestaurantList /> */}
+      {data.state.map((value, i) => (
+        <RestaurantList key={i} state={value} restaurants={filterByState(value)}/>
+      ))}
     </div>
   );
 };
--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -1,45 +1,77 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
 
-import '../shared/css/RestaurantList.css'
+import "../shared/css/RestaurantList.css";
+import { RestaurantProps } from "./Restaurant";
 
-export const RestaurantList = () => {
+type RestaurantListProp = {
+  state: string;
+  restaurants: RestaurantProps[];
+};
+
+export const RestaurantList = (props: RestaurantListProp) => {
+  const listContainerRef = useRef<HTMLDivElement>(null);
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    if (listContainerRef.current) {
+      if (toggle) {
+        // const height = (props.restaurants.length - 1) * 55 + 73;
+        // listContainerRef.current.style.height = `${height.toString()}px`;
+        listContainerRef.current.style.height = "auto";
+        listContainerRef.current.classList.add('show');
+
+      } else {
+        listContainerRef.current.classList.remove('show');
+        listContainerRef.current.style.height = "43px";
+      }
+    }
+  }, [toggle]);
+
+  // useEffect(() => {
+  //   const setContainerHeight = () => {
+  //     if (listContainerRef.current) {
+  //       const height = toggle ? (props.restaurants.length - 1) * 55 + 73 : 43;
+  //       listContainerRef.current.style.height = `${height}px`;
+  //     }
+  //   };
+
+  //   setContainerHeight();
+  // }, [toggle, props.restaurants.length]);
+
   return (
-    <div className='restaurant-list'>
-        <div className="header">
-            <div className='header-content-1'>
-                <span className="icon glyphicon glyphicon-glass"></span>
-                <span className="state">Georgia</span>
-            </div>
-            <div className='header-content-2'>
-                <span className="test">3</span>
-            </div>
+    <div className="restaurant-list">
+      <div className="header">
+        <div className="header-content-1">
+          <span className="icon glyphicon glyphicon-map-marker"></span>
+          <span className="state">{props.state}</span>
         </div>
-        <div className="content">
-          <div className="list">
-              <span className="list-name">Green Leaf 1</span>
-              <div className='list-review'>
-                <span className="glyphicon glyphicon-star"></span>
-                <span className="glyphicon glyphicon-star"></span>
-                <span className="glyphicon glyphicon-star"></span>
-              </div>
-          </div>
-          <div className="list">
-              <span className="list-name">Green Leaf 1</span>
-              <div className='list-review'>
-                <span className="glyphicon glyphicon-star"></span>
-                <span className="glyphicon glyphicon-star"></span>
-                <span className="glyphicon glyphicon-star"></span>
-              </div>
-          </div>
-          <div className="list">
-              <span className="list-name">Green Leaf 1</span>
-              <div className='list-review'>
-                <span className="glyphicon glyphicon-star"></span>
-                <span className="glyphicon glyphicon-star"></span>
-                <span className="glyphicon glyphicon-star"></span>
-              </div>
-          </div>
+        <div className="header-content-2">
+          <span className="test">{props.restaurants.length}</span>
         </div>
+      </div>
+      <div className="content" ref={listContainerRef}>
+        {toggle &&
+          props.restaurants.map((restaurant, i) => (
+            <div className="list" key={i}>
+              <span className="list-name">{restaurant.restaurant_name}</span>
+              <div className="list-review">
+                <span className="glyphicon glyphicon-star"></span>
+                <span className="glyphicon glyphicon-star"></span>
+                <span className="glyphicon glyphicon-star"></span>
+                <span className="glyphicon glyphicon-star"></span>
+                <span className="glyphicon glyphicon-star"></span>
+              </div>
+            </div>
+          ))}
+        <div className="action">
+          <button
+            className="btn btn-light btn-lg"
+            onClick={() => setToggle(!toggle)}
+          >
+            {toggle ? 'hide' : 'view more'}
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
